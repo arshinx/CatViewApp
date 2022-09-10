547e0e5d3623e3ef0b6f1debf065ecc08e3f3502
Arshin Jain <arshinx@users.noreply.github.com>
2022-09-10 18:40:12 -0400

    Update BreedsListView.swift

--- CatViewApp/Views/BreedsListView.swift
+++ CatViewApp/Views/BreedsListView.swift
@@ -5,3 +5,24 @@
 //  Created by Arshin Jain on 9/10/22.
 //
 
+import SwiftUI
+
+struct BreedsListView: View {
+    let breeds: [Breed]
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct BreedsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BreedsListView(breeds: [])
+    }
+}
+
