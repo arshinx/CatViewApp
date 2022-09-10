a23944ccc4bf54ea6a073647b002e8781664133e
Arshin Jain <arshinx@users.noreply.github.com>
2022-09-10 18:40:16 -0400

    Update ContentView.swift

--- CatViewApp/ContentView.swift
+++ CatViewApp/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var breedFetcher = BreedFetcher()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if breedFetcher.isLoading {
+                LoadingView()
+            } else if breedFetcher.errorMessage != nil {
+                ErrorView(breedFetcher: breedFetcher)
+            } else {
+                BreedsListView(breeds: breedFetcher.breeds)
+            }
         }
         .padding()
     }
