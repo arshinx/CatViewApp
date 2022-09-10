fe7d261fb0a750a12fd848cde7e33858558d3b4b
Arshin Jain <arshinx@users.noreply.github.com>
2022-09-10 11:22:12 -0400

    Initial Commit

---
+++ CatViewApp/CatViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatViewAppApp.swift
+//  CatViewApp
+//
+//  Created by Arshin Jain on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CatViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
