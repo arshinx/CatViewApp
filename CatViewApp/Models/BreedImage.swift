a43b5688b7ae0bb0ffbb13554ad556756003fe28
Arshin Jain <arshinx@users.noreply.github.com>
2022-09-10 18:39:35 -0400

    Update BreedImage.swift

--- CatViewApp/Models/BreedImage.swift
+++ CatViewApp/Models/BreedImage.swift
@@ -4,3 +4,14 @@
 //
 //  Created by Arshin Jain on 9/10/22.
 //
+import UIKit
+
+struct BreedImage: Codable {
+    let height: Int
+    let id: String
+    let url: String
+    let width: Int
+    var getURL: URL? {
+        URL(string: url)
+    }
+}
